cf68b84987a04ddb775723e4f1d63edae17d486f
jxm930 <jxm930@gmail.com>
2018-02-11 17:01:10 +0900

    custome list view clear

--- app/src/main/java/test/ki/mypractice/CustomAdapter.java
+++ app/src/main/java/test/ki/mypractice/CustomAdapter.java
@@ -1,7 +1,6 @@
 package test.ki.mypractice;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +14,25 @@ public class CustomAdapter extends BaseAdapter {
     LayoutInflater layoutInflater = null;
     List<CustomFoodList> foodList;
 
-    public CustomAdapter (Context context, List<CustomFoodList> data) {
-        foodList = data;
+    public CustomAdapter (Context context) {
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setFoodList(List<CustomFoodList> data) {
+        this.foodList = data;
+    }
+
+    static class ViewHolder {
+
+        TextView content1;
+        TextView content2;
+        TextView content3;
+
+        ViewHolder(View view) {
+            this.content1 = view.findViewById(R.id.custom_list_row_col1);
+            this.content2 = view.findViewById(R.id.custom_list_row_col2);
+            this.content3 = view.findViewById(R.id.custom_list_row_col3);
+        }
     }
 
     @Override
@@ -27,7 +42,9 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public String getItem(int position) {
-        return foodList.get(position).getName();
+        CustomFoodList food = foodList.get(position);
+        String showStr = food.getId() + "-" + food.getName() + "(" + food.getPrice() + ")";
+        return showStr;
     }
 
     @Override
@@ -38,18 +55,20 @@ public class CustomAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View view = layoutInflater.inflate(R.layout.custom_list_one_row_item, null);
-        Log.e("### DEBUG TAG ###", this.getClass().toString() + "getView.");
+        ViewHolder holder;
 
-        TextView conView1 = view.findViewById(R.id.custom_list_row_col1);
-        conView1.setText(String.valueOf(foodList.get(position).getId()));
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.custom_list_one_row_item, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-        TextView conView2 = view.findViewById(R.id.custom_list_row_col2);
-        conView2.setText(foodList.get(position).getName());
+        holder.content1.setText(String.valueOf(foodList.get(position).getId()));
+        holder.content2.setText(String.valueOf(foodList.get(position).getName()));
+        holder.content3.setText(String.valueOf(foodList.get(position).getPrice()));
 
-        TextView conView3 = view.findViewById(R.id.custom_list_row_col3);
-        conView3.setText(String.valueOf(foodList.get(position).getPrice()));
-
-        return view;
+        return convertView;
     }
 }
--- app/src/main/java/test/ki/mypractice/CustomListViewActivity.java
+++ app/src/main/java/test/ki/mypractice/CustomListViewActivity.java
@@ -2,15 +2,19 @@ package test.ki.mypractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-public class CustomListViewActivity extends AppCompatActivity {
+public class CustomListViewActivity extends AppCompatActivity implements View.OnClickListener,ListView.OnItemClickListener {
 
     ListView listview1;
+    CustomAdapter customAdapter;
+    ArrayList<CustomFoodList> foods;
+    TextView tv_SelectedShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +23,7 @@ public class CustomListViewActivity extends AppCompatActivity {
 
         listview1 = findViewById(R.id.custom_list_1);
 
-        ArrayList<CustomFoodList> foods = new ArrayList<>();
+        foods = new ArrayList<>();
 
         CustomFoodList cf1 = new CustomFoodList();
         cf1.setId(Long.valueOf(1));
@@ -33,9 +37,61 @@ public class CustomListViewActivity extends AppCompatActivity {
         cf2.setPrice(129);
         foods.add(cf2);
 
-        CustomAdapter customAdapter = new CustomAdapter(CustomListViewActivity.this, foods);
+        customAdapter = new CustomAdapter(CustomListViewActivity.this);
+        customAdapter.setFoodList(foods);
         listview1.setAdapter(customAdapter);
 
+        CustomFoodList cf3 = new CustomFoodList();
+        cf3.setId(Long.valueOf(3));
+        cf3.setName("オレンジ");
+        cf3.setPrice(500);
+        foods.add(cf3);
+
+        CustomFoodList cf4 = new CustomFoodList();
+        cf4.setId(Long.valueOf(4));
+        cf4.setName("マンゴ");
+        cf4.setPrice(290);
+        foods.add(cf4);
+        customAdapter.setFoodList(foods);
+//        customAdapter.notifyDataSetChanged();
+
+        tv_SelectedShow = this.findViewById(R.id.item_show);
+        this.findViewById(R.id.add_item).setOnClickListener(this);
+        this.findViewById(R.id.delete_item).setOnClickListener(this);
+        listview1.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int btnId = v.getId();
+
+        switch (btnId) {
+            case R.id.add_item:
+
+                CustomFoodList cf3 = new CustomFoodList();
+                cf3.setId(Long.valueOf(foods.size() + 1));
+                cf3.setName("オレンジ");
+                cf3.setPrice(500);
+                foods.add(cf3);
+
+                break;
+            case R.id.delete_item:
+                foods.remove(foods.size() -1);
+                break;
+        }
+
+        customAdapter.setFoodList(foods);
         customAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        ListView listView = (ListView) parent;
+
+        // ここは、CustomeAdapterのgetItemが実装されます
+        String item = (String) listView.getItemAtPosition(position);
+        tv_SelectedShow.setText(item);
     }
 }
